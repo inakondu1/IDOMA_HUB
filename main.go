aa0e25d42ff2765cf6fa3feac3dfca10ac68a1f4
inakondu1 <inaisah66@gmail.com>
2026-09-21 17:01:53 +0100

    Build IDOMA HUB community homepage

--- main.go
+++ main.go
@@ -32,8 +32,30 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func registerPageHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/register.html")
+	if err != nil {
+		http.Error(w, "Unable to load registration page", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	data := PageData{
+		Title: "Create Account - IDOMA HUB",
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Unable to display registration page", http.StatusInternalServerError)
+		log.Println(err)
+	}
+}
+
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/register", registerPageHandler)
 
 	fmt.Println("IDOMA HUB is running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
